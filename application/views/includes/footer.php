f80ec8caace8a16dc85423304b899933c4736848
aisahsya <aisahnuraini815@gmail.com>
2024-02-27 17:17:28 +0700

    first commit

---
+++ application/views/includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+<strong>UKK 2024 &copy;<a href="http://adminlte.io">Aisah Nuraini</a>.</strong>
+
+<div class="float-right d-none d-sm-inline-block">
+  <b>XII RPL 2</b> 
+</div>
+</footer>
